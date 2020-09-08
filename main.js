44b1540b5f27894693dab494c67de2145ece0aab
Aaron Townsend <atownse17@gmail.com>
2020-09-07 19:58:45 -0500

    Completed and functioning Iteration 4

--- main.js
+++ main.js
@@ -1,35 +1,40 @@
 var sides = [
-    "Miso Glazed Carrots", 
-    "Coleslaw", 
     "Garden Salad", 
     "Crispy Potatoes", 
     "Sweet Potato Tots", 
     "Coconut Rice", 
     "Caesar Salad", 
     "Shrimp Summer Rolls", 
-    "Garlic Butter Mushrooms"]
+    "Garlic Butter Mushrooms",
+    "Nachos",
+    "Mashed Potatoes",
+    "Egg Rolls"]
 
 var mainDishes = [
-    "Spaghetti and Meatballs", 
     "Pineapple Chicken", 
     "Shakshuka", 
     "Thai Yellow Curry", 
     "Bibmbap", 
     "Shrimp Alfredo Fettucine", 
-    "Chicken Parmesean", 
-    "Butternut Squash Soup", 
+    "Chicken Parmesean",  
     "BBQ Chicken Burgers", 
-    "Ramen", "Empanadas", 
+    "Ramen", 
+    "Empanadas", 
     "Chicken Fried Rice", 
     "Sheet Pan Fajitas", 
-    "Margarita Pizza"]
+    "Margarita Pizza",
+    "Shrimp Creole",
+    "Paella",
+    "Beef Fajitas",
+    "Jumbalaya"]
 
 var desserts = [
-    "Apple Pie", 
-    "Lemon Meringue Pie", 
+    "Brownies",
+    "Faerie Bread",
+    "Chocolate Shake",
+    "Chocolate Chip Cookies", 
     "Black Forest Cake", 
     "Banana Bread", 
-    "Peach Cobbler", 
     "Best Chocolate Cake Ever", 
     "Cheesecake", 
     "Funfetti Cake", 
@@ -38,7 +43,7 @@ var desserts = [
     "Macarons", 
     "Macaroons", 
     "Chocolate Cupcakes", 
-    "Pavlova", "Pumpkin Pie", 
+    "Pavlova", 
     "Key Lime Pie", 
     "Tart Tatin", 
     "Croissants", 
@@ -48,10 +53,14 @@ var desserts = [
 letsCookButton = document.querySelector(".cook-button")
 resultFood = document.getElementById("result-text")
 clearButton = document.querySelector(".empty-out")
+addButton = document.querySelector(".button")
+newRecipeButton = document.getElementById("new-addition")
 
 // Event Listeners
 letsCookButton.addEventListener("click", generateRandomFood)
 clearButton.addEventListener("click",displayCookPot)
+addButton.addEventListener("click", displayCreateDish)
+newRecipeButton.addEventListener("click", addNewRecipe)
 
 // Functions
 function getRandomIndex(array) {
@@ -77,17 +86,36 @@ function clearRadio() {
 
 function generateRandomFood() {
     if (document.getElementById("side").checked) {
+        resultFood.innerText = `${sides[getRandomIndex(sides)]}!`
         changeDisplay()
-        resultFood.innerText = `${sides[getRandomIndex(sides)]}`
     } else if (document.getElementById("main-dish").checked) {
-        changeDisplay()
         resultFood.innerText = `${mainDishes[getRandomIndex(mainDishes)]}!`
+        changeDisplay()
     } else if (document.getElementById("dessert").checked) {
+        resultFood.innerText = `${desserts[getRandomIndex(desserts)]}!`
         changeDisplay()
-        resultFood.innerText = `${desserts[getRandomIndex(desserts)]}`
     } else if (document.getElementById("entire-meal").checked) {
+        resultFood.innerText = `${mainDishes[getRandomIndex(mainDishes)]} with a side of ${sides[getRandomIndex(sides)]} and ${desserts[getRandomIndex(desserts)]} for dessert!`
         changeDisplay()
-        resultFood.innerText = `${mainDishes[getRandomIndex(mainDishes)]} with a side of ${sides[getRandomIndex(sides)]} for dessert!`
     }
+}
 
+function displayCreateDish() {
+    document.querySelector(".own-recipe").classList.toggle("hidden")
+}
+
+function addNewRecipe() {
+    var newRecipeName = document.getElementById("recipe-name")
+    var newRecipeType = document.getElementById("recipe-type").selectedIndex
+    var optionHTML = document.getElementsByTagName("option") 
+    if (optionHTML[newRecipeType].value === "side") {
+        sides.push(newRecipeName.value)
+    } else if (optionHTML[newRecipeType].value === "main-dish") {
+        mainDishes.push(newRecipeName.value)
+    } else if (optionHTML[newRecipeType].value === "dessert") {
+        desserts.push(newRecipeName.value)
+    }
+    resultFood.innerText = `${newRecipeName.value}!`
+    changeDisplay()
+    displayCreateDish()
 }
